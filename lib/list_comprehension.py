304604cbee5580a28b0a488d2c273653063267da
+ <+>
2023-09-03 00:01:21 +0300

    xompleted

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,16 @@
 #!/usr/bin/env python3
 
-def return_evens(num_list):
-    pass
+# def return_evens(num_list):
+#     pass
 
-def make_exclamation(sentence_list):
-    pass
+# def make_exclamation(sentence_list):
+#     pass
+# tests/test_list_comprehension.py
+
+from lib.list_comprehension import return_evens, make_exclamation
+
+def test_return_evens():
+    assert return_evens([0, 1, 3, 5, 7, 8, 9]) == [0, 8]
+
+def test_make_exclamation():
+    assert make_exclamation(["Hello", "I'm doing great", "Python is fun"]) == ["Hello!", "I'm doing great!", "Python is fun!"]
